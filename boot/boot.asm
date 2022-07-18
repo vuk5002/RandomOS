3c8d4b800aad4fe47b544fbe8358083819ca210b
vuk5002 <luckyhackers3@gmail.com>
2022-07-18 15:05:33 +0200

    Better print!
    Now it prints a whole string!

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,11 +1,24 @@
+org 0x7c00
 ; Main bootloader
 ; loads FIRST 512 bytes (510 + padding)
 ; Hidden Studios (c) 2020-2022 All Rights Reserved.
 
-mov ah, 0x0e
-mov al, 'H'
+mov si, string
+call print
 
-int 0x10
+print:
+    mov ah, 0x0e
+    mov bh, 0x00
+    .loop:
+        lodsb
+        cmp al, 0x00
+        je .exit
+        int 0x10
+        jmp .loop
+  .exit:
+       ret
+
+string: db "Welcome to RandomOS!", 0
 
 ; boot signature
 times 510-($-$$) db 0
