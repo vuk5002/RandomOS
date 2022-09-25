64cbf5f1d99faa052f9e03c2648b400139deeadc
vuk5002 <luckyhackers3@gmail.com>
2022-09-25 19:10:07 +0200

    BONUS! Giving bios something to read

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -80,6 +80,15 @@ main:
     mov ss, ax
     mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
+    ; read something from floppy
+    ; BIOS should set DL to drive number
+    mov [ebr_drive_number], dl
+
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
     ; print hello world message
     mov si, msg_hello
     call puts
